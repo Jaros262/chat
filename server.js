f04e248d0760c38bbe6e068e497f4742251d3b56
Matěj Jaroš <matysekjaros@gmail.com>
2021-03-23 12:13:46 +0100

    rozhraní

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const io = socketio(server);
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-const botName = 'Chatter Bot';
+const botName = 'Witchat Bot';
 
 // Run when client connects
 io.on('connection', socket => {
